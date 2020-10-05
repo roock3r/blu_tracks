9622a3a76323da507d5bcc51c3faec4a2d692146
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-10-05 14:57:21 -0600

    Added dynamic variables based on credentials py.

--- blu_tracks/settings.py
+++ blu_tracks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import credentials
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-g6kost^f2160viarn_vf^!x3lhyjenfje=@9hy6o-i&q4rlbb'
+SECRET_KEY = credentials.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = credentials.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [credentials.ALLOWED_HOST, credentials.ALLOWED_HOST1,
+                 credentials.ALLOWED_HOST2, credentials.ALLOWED_HOST3]
 
 
 # Application definition
@@ -62,7 +64,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    credentials.CORS_HOST_1,credentials.CORS_HOST_2
 ]
 
 ROOT_URLCONF = 'blu_tracks.urls'
@@ -89,13 +91,26 @@ WSGI_APPLICATION = 'blu_tracks.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': credentials.DBNAME,
+            'USER': credentials.DBUSER,
+            'PASSWORD': credentials.DBPASSWORD,
+            'HOST': credentials.DBHOME,
+            'PORT': credentials.DBPORT,
+            'OPTIONS': {'sslmode': credentials.SSL},
+        }
+    }
+
 
 
 # Password validation
