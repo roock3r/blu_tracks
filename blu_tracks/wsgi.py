a7acb749cde67d61e9363ce5b0239a4004216003
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-10-05 15:30:55 -0600

    Remove extra space causing error.

--- blu_tracks/wsgi.py
+++ blu_tracks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', ' blu_tracks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blu_tracks.settings')
 
 application = get_wsgi_application()
